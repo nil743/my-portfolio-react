b340f48957650941c50fdb67c5664f4b9bad5ef4
nil743 <71000086+nil743@users.noreply.github.com>
2023-09-16 23:41:45 +0530

    Create index.js

---
+++ src/components/Home/index.js
@@ -0,0 +1,79 @@
+import { useEffect, useState } from 'react'
+import { Link } from 'react-router-dom'
+import Loader from 'react-loaders'
+import AnimatedLetters from '../AnimatedLetters'
+import LogoTitle from '../../assets/images/Logo-N.png'
+//import Logo from './Logo'
+import './index.scss'
+
+const Home = () => {
+  const [letterClass, setLetterClass] = useState('text-animate')
+
+  const nameArray = ['i', 'l', 'a', 'v', 'a']
+  const jobArray = [
+    'S',
+    'o',
+    'f',
+    't',
+    'w',
+    'a',
+    'r',
+    'e',
+    ' ',
+    'E',
+    'n',
+    'g',
+    'i',
+    'n',
+    'e',
+    'e',
+    'r',
+    '.',
+  ]
+
+  useEffect(() => {
+    return () => { setTimeout(() => {
+      setLetterClass('text-animate-hover')
+    }, )
+  }}, [])
+
+  return (
+    <>
+      <div className="container home-page">
+        <div className="text-zone">
+          <h1>
+            <span className={letterClass}>H</span>
+            <span className={`${letterClass} _12`}>i,</span>
+            <br />
+            <span className={`${letterClass} _13`}>I</span>
+            <span className={`${letterClass} _14`}>'m</span>
+            <img
+              src={LogoTitle}
+              alt="JavaScript Developer Name, Web Developer Name"
+            />
+            <AnimatedLetters
+              letterClass={letterClass}
+              strArray={nameArray}
+              idx={15}
+            />
+            <br />
+            <AnimatedLetters
+              letterClass={letterClass}
+              strArray={jobArray}
+              idx={22}
+            />
+          </h1>
+          <h2>IAM Developer / Full-Stack Developer </h2>
+          <Link to="/contact" className="flat-button">
+            CONTACT ME
+          </Link>
+        </div>
+        
+      </div>
+
+      <Loader type="pacman" />
+    </>
+  )
+}
+
+export default Home
